a24977caf709634e4288d60fa2d07791bd1dfad1
daniel130300 <daniel130300@outlook.com>
2021-10-21 12:39:40 -0600

    added the tab navbar but still needs the partials

--- app/javascript/packs/my_js.js
+++ app/javascript/packs/my_js.js
@@ -1,14 +1,26 @@
 //console.log('Hello from My JS')
 
-// window.addEventListener("load", () => {
-//     const element = document.querySelector("#search-book");
-//     const books_results = document.querySelector("#books-results");
-//     const errors = document.querySelector("#errors")
-//     element.addEventListener("ajax:success", (event) => {
-//       const [_data, _status, xhr] = event.detail;
-//       books_results.insertAdjacentHTML("beforeend", xhr.responseText);
-//     });
-//     element.addEventListener("ajax:error", () => {
-//       element.insertAdjacentHTML("beforeend", "<p>ERROR</p>");
-//     });
-// });
+window.addEventListener("DOMContentLoaded", () => {
+    const owned_tab = document.querySelector("#owned-tab");
+    const owned_tab_content = document.querySelector('#owned');
+    const wishlist_tab = document.querySelector("#wishlist-tab");
+    const wishlist_tab_content = document.querySelector('#wishlist');
+
+    owned_tab.addEventListener('click', ()=> {
+        //Being showed
+        owned_tab_content.classList.toggle('active');
+        owned_tab_content.classList.toggle('show');
+        //Hide
+        wishlist_tab_content.classList.toggle('active');
+        wishlist_tab_content.classList.toggle('show');
+    })
+
+    wishlist_tab.addEventListener('click', ()=> {
+        //Being showed
+        wishlist_tab_content.classList.toggle('active');
+        wishlist_tab_content.classList.toggle('show');
+        //Hide
+        owned_tab_content.classList.toggle('active');
+        owned_tab_content.classList.toggle('show');
+    })
+});
